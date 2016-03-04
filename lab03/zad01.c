ac7db75b4dd8881c5fd16c58a361f3dade4da27f
Piotr Aszkielaniec <readher@gmail.com>
2016-03-04 11:28:06 +0100

    modified excersise

--- lab03/zad01.c
+++ lab03/zad01.c
@@ -8,8 +8,8 @@ int main()
   printf("int:\nDolna wartosc: %i\nGorna wartosc: %i\nZiarno: -\nPrecyzja: -\n\n", INT_MIN, INT_MAX);
   printf("long:\nDolna wartosc: %li\nGorna wartosc: %li\nZiarno: -\nPrecyzja: -\n\n", LONG_MIN, LONG_MAX);
   printf("long long:\nDolna wartosc: %lli\nGorna wartosc: %lli\nZiarno: -\nPrecyzja: -\n\n", LLONG_MIN, LLONG_MAX);
-  printf("float:\nDolna wartosc: %f\nGorna wartosc: %f\nZiarno: %f\nPrecyzja: %f\n\n", FLT_MIN, FLT_MAX, FLT_EPSILON, FLT_DIG);
-  printf("double:\nDolna wartosc: %lf\nGorna wartosc: %lf\nZiarno: %lf\nPrecyzja: %lf\n\n", DBL_MIN, DBL_MAX, DBL_EPSILON, DBL_DIG);
-  printf("long double:\nDolna wartosc: %Lf\nGorna wartosc: %Lf\nZiarno: %Lf\nPrecyzja: %Lf\n\n", LDBL_MIN, LDBL_MAX, LDBL_EPSILON, LDBL_DIG);
+  printf("float:\nDolna wartosc: %f\nGorna wartosc: %f\nZiarno: %f\nPrecyzja: %i\n\n", FLT_MIN, FLT_MAX, FLT_EPSILON, FLT_DIG);
+  printf("double:\nDolna wartosc: %lf\nGorna wartosc: %lf\nZiarno: %lf\nPrecyzja: %i\n\n", DBL_MIN, DBL_MAX, DBL_EPSILON, DBL_DIG);
+  printf("long double:\nDolna wartosc: %Lf\nGorna wartosc: %Lf\nZiarno: %Lf\nPrecyzja: %i\n\n", LDBL_MIN, LDBL_MAX, LDBL_EPSILON, LDBL_DIG);
   return 0;
 }
