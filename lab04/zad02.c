6159b8429aeca399eee19a775b55ec9dfc5fdc8a
readher <readher@gmail.com>
2016-03-11 11:02:47 +0100

    Update zad02.c

--- lab04/zad02.c
+++ lab04/zad02.c
@@ -1,1 +1,10 @@
+//Poniższa tożsamość Eulera w prosty sposób łączy ze sobą 5 ważnych stałych:
+//    eπi + 1  =  0 
+//dlatego przez niektórych jest uważana za ,,najpiękniejszy'' wzór matematyczny.
+//Program z zadania 1 wzbogacić o funkcje potrzebne do tego, żeby można było sprawdzić, czy tożsamość Eulera istotnie zachodzi (mogą wystąpić błędy zaokrągleń).
+//Podnoszenie e do potęgi urojonej można zdefiniować tak:
+//    eri  =  cos(r) + sin(r)i 
+//Podnoszenie e do potęgi zespolonej (niekoniecznie czysto urojonej) można zdefiniować tak:
+//    ez  =  ∑k=0∞ zk/k! 
+
 
