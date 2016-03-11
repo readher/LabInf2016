064251f06d99f084787175655f6ca3aefd9069e0
readher <readher@gmail.com>
2016-03-11 10:52:40 +0100

    Update zad06.c

--- lab01/zad06.c
+++ lab01/zad06.c
@@ -1,3 +1,10 @@
+//Sprawdzić i wyjaśnić działanie następujących fragmentów programów:
+//a = b = c = d = 0;
+//for (i=0; i<N; a[i++]=i);
+//for (i=0; i<N; a[++i]=i);
+//i=1; while ((i*=2)<N);
+
+
 #include<stdio.h>
 
 int main()
