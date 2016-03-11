7682f8c1ba4edc641e6324164d75b6acd19ca1b2
readher <readher@gmail.com>
2016-03-11 11:01:54 +0100

    Update zad01.c

--- lab04/zad01.c
+++ lab04/zad01.c
@@ -1,1 +1,3 @@
-
+//Opracować zestaw operacji na liczbach zespolonych potrzebny do prawidłowego działania tego programu.
+//W tym celu należy prototypy (nagłówki) podanych tam funkcji, realizujących działania, zastąpić lub uzupełnić pełnymi definicjami działań.
+//Oczywiście dla napisania tych funkcji, potrzebne jest zastosowanie własnej wiedzy o liczbach zespolonych (np. z algebry).
