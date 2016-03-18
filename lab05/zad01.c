ae09ac379dbc36fb25d5ca46bfb0fba752a1162c
Piotr Aszkielaniec <readher@gmail.com>
2016-03-18 11:17:37 +0100

    added zad01 to lab05

---
+++ lab05/zad01.c
@@ -0,0 +1,24 @@
+//Napisać funkcję typu void, która ma
+    //dwa całkowite parametry wejściowe n i k, k≠0,
+    //dwa całkowite parametry wyjściowe:
+        //q równy ilorazowi całkowitemu n przez k, oraz
+        //r równy reszcie z dzielenia n przez k.
+
+//Ta funkcja nie może mieć żadnych innych parametrów, nie może odwoływać się do zmiennych globalnych i oczywiście nie może niczego czytać ani pisać -- jej jedyna komunikacja z resztą programu odbywa się przez parametry.
+//Napisać program, który wczytuje dwie liczby całkowite i wywołuje powyższą funkcję, a następnie drukuje iloraz całkowity oraz resztę z dzielenia wczytanych liczb.
+
+#include<stdio.h>
+
+void funkcja(int n, int k, int *q, int *r)
+{
+  *q=(n/k);
+  *r=(n%k);
+}
+
+int main()
+{
+  int iloraz, reszta, l1, l2;
+  scanf("%d %d", &l1, &l2);
+  funkcja(l1, l2, &iloraz, &reszta);
+  printf("Iloraz: %d\nReszta: %d\n", iloraz, reszta);
+}
