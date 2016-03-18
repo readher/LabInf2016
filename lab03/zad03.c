9340320e0c240eede61e6369d3d6e423665ad88e
Piotr Aszkielaniec <readher@gmail.com>
2016-03-18 10:24:25 +0100

    added lab05

--- lab03/zad03.c
+++ lab03/zad03.c
@@ -6,7 +6,7 @@
 
 int main()
 {
-	printf("FLT_MAX dla int: %i", FLT_MAX);
-	printf("DBL_MAX dla int: %i", DBL_MAX);
+	printf("FLT_MAX dla int: %i\n", FLT_MAX);
+	printf("DBL_MAX dla int: %i\n", DBL_MAX);
 	printf("LDBL_MAX dla double: %lf", LDBL_MAX);
 }
