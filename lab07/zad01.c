d407f54e5d7eeb196657031b3550d92f777616a2
Piotr Aszkielaniec <readher@gmail.com>
2016-05-06 10:43:33 +0200

    Update

--- lab07/zad01.c
+++ lab07/zad01.c
@@ -2,15 +2,16 @@
  //./od_konca  Ty. gdzie tam, zawsze tam, Byc
 //wydrukuje tekst
  //Byc tam, zawsze tam, gdzie Ty.
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int ile_arg, char* arg[ ]) {
+int main (int ile_arg, char* arg[ ])
+{
     int i;
-    for(i=ile_arg-1; i>0; i--){
+    for(i=ile_arg-1; i>0; i--)
+    {
         printf(" %s", arg[i]);
-        }
-printf("\n");
-return 0;
+    }
+    printf("\n");
 }
